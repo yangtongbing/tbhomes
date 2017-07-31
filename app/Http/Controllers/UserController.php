020b543095ec662438995896062d62cead04f0e4
yangtongbing <yangtongbing@haodai.net>
2017-07-31 11:36:24 +0800

    '初始化'

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 12807
+ * Date: 2017/6/8
+ * Time: 18:21
+ */
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller{
+    public function show(){
+        exit("123");
+    }
+}
