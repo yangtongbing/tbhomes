ce49bbf40948bccc38eaa1bf1fa7dca28a3290a0
yangtongbing <yangtongbing@haodai.net>
2017-08-01 10:45:07 +0800

    '修改使用类'

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
