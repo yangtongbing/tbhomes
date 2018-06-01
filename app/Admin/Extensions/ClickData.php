9fb00c690e076e9d608386a5cd1491606d264ce4
yangtongbing <yangtongbing@haodai.com>
2018-06-01 15:43:18 +0800

    修改之前不能复制的问题，传入对象有问题，重新获取不能进行复制操作

--- app/Admin/Extensions/ClickData.php
+++ app/Admin/Extensions/ClickData.php
@@ -19,9 +19,8 @@ SCRIPT;
         Admin::script($this->script());
         return "
             <script>
-            function clickData(){
-                var Url2=document.getElementById('clickData');
-                Url2.select(); // 选择对象
+            function clickData(obj){
+                obj.select(); // 选择对象
                 document.execCommand('Copy'); // 执行浏览器复制命令
                 alert('已复制好，可贴粘。');
             }            
